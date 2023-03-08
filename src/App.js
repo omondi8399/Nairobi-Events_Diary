ed89ce2329aba8f09999661b934a647051c0a515
Rodgers Omondi <rodgersomondi8399@gmail.com>
2023-03-08 19:26:38 +0300

    Updated code

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import EditEventPage from './pages/EditEvent'
+import NewEventPage from './pages/NewEvent'
+import EventDetailPage from './pages/EventDetail'
+import EventsPage from './pages/Events';
+import HomePage from './pages/Home';
+
+const router = createBrowserRouter([
+  { path: '/', element: <HomePage />},
+  { Path: '/events', element: <EventsPage />},
+  { path: '/events/:eventId', element: <EventDetailPage />},
+  { path: '/events/new', element: <NewEventPage />},
+  { path: '/events/:eventId/edit', element: <EditEventPage />},
+])
+
 function App() {
-  return <div></div>;
+  return (
+    <RouterProvider router={router} />
+  )
 }
 
 export default App;
---
+++ src/pages/EditEvent.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const EditEventPage = () => {
+  return (
+    <div>
+      <h1>EditEventPage</h1>
+    </div>
+  )
+}
+
+export default EditEventPage
